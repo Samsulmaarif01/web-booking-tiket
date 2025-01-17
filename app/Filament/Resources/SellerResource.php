95bf7e6ab72ad3a144bc37e3fd33addc512fc37c
samsulmaarif1076 <samsulmaarif1076@gmail.com>
2025-01-17 08:54:47 +0000

    CRUD Seller data

--- app/Filament/Resources/SellerResource.php
+++ app/Filament/Resources/SellerResource.php
@@ -6,9 +6,13 @@ use App\Filament\Resources\SellerResource\Pages;
 use App\Filament\Resources\SellerResource\RelationManagers;
 use App\Models\Seller;
 use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -19,11 +23,27 @@ class SellerResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $navigationGroup = 'Management';
+
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')
+                ->required()
+                ->maxLength(255),
+
+                TextInput::make('telephone')
+                ->required()
+                ->maxLength(255),
+
+                TextInput::make('location')
+                ->required()
+                ->maxLength(255),
+
+                FileUpload::make('photo')
+                ->image()
+                ->required(),
             ]);
     }
 
@@ -31,13 +51,25 @@ class SellerResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')
+                ->searchable(),
+
+                TextColumn::make('location')
+                ->searchable(),
+
+                TextColumn::make('telephone')
+                ->searchable(),
+
+                ImageColumn::make('photo')
+                ->circular(),
+
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
