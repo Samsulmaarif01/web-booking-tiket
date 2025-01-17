c27df92ad3c6eaa558bb4afe1fa3d9ca1f5a1993
samsulmaarif1076 <samsulmaarif1076@gmail.com>
2025-01-17 07:31:24 +0000

    feat: make model and set migration

--- database/migrations/2025_01_17_072245_create_sellers_table.php
+++ database/migrations/2025_01_17_072245_create_sellers_table.php
@@ -13,9 +13,18 @@ return new class extends Migration
     {
         Schema::create('sellers', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+            $table->string('telephone');
+            $table->string('location');
+            $table->string('slug');
+            $table->string('photo');
+            $table->softDeletes();
+
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
--- database/migrations/2025_01_17_072823_create_ticket_photos_table.php
+++ database/migrations/2025_01_17_072823_create_ticket_photos_table.php
@@ -11,18 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('booking_transactions', function (Blueprint $table) {
+        Schema::create('ticket_photos', function (Blueprint $table) {
             $table->id();
 
-            $table->string('name');
-            $table->string('booking_trx_id');
-            $table->string('phone_number');
-            $table->string('email');
-            $table->string('proof');
-            $table->unsignedBigInteger('total_amount');
-            $table->unsignedBigInteger('total_participant');
-            $table->boolean('is_paid');
-            $table->date('started_at');
+            $table->string('photo');
             $table->foreignId('ticket_id')->constrained()->cascadeOnDelete();
             $table->softDeletes();
 
@@ -35,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('booking_transactions');
+        Schema::dropIfExists('ticket_photos');
     }
 };
